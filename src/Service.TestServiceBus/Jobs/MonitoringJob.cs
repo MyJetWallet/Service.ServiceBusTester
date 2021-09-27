eeecab53785675308c1692efaf5989018873ebea
alexey <alexey.n@smplt.net>
2021-09-27 14:00:14 +0300

    log exceptions

--- src/Service.TestServiceBus/Jobs/MonitoringJob.cs
+++ src/Service.TestServiceBus/Jobs/MonitoringJob.cs
@@ -68,45 +68,64 @@ namespace Service.TestServiceBus.Jobs
 
         private async ValueTask BidAskCallback(IMyServiceBusMessage message, string topic)
         {
-            long lasted;
-
-            lock(_lasted) lasted = _lasted[topic];
-            
-            if (lasted > 0 && lasted != message.Id - 1)
+            try
             {
-                Console.WriteLine($"Wrong ID, Topic {topic}. Receive Id = {message.Id}, but lasted = {lasted}");
-                await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId, $"{Program.Settings.Name}] Wrong ID, Topic {topic}. Receive Id = {message.Id}, but lasted = {lasted}");
-            }
+                long lasted;
 
-            lock (_lasted) _lasted[topic] = message.Id;
+                lock (_lasted) lasted = _lasted[topic];
+
+                if (lasted > 0 && lasted != message.Id - 1)
+                {
+                    Console.WriteLine($"Wrong ID, Topic {topic}. Receive Id = {message.Id}, but lasted = {lasted}");
+                    await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId,
+                        $"{Program.Settings.Name}] Wrong ID, Topic {topic}. Receive Id = {message.Id}, but lasted = {lasted}");
+                }
+
+                lock (_lasted) _lasted[topic] = message.Id;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception on single handler for {topic}\n{e}");
+            }
         }
 
         private async ValueTask BidAskBatchCallback(IReadOnlyList<IMyServiceBusMessage> messages, string topic)
         {
-            if (messages.Count == 0)
-                return;
-
-            long lasted;
-            lock (_lasted) lasted = _lasted[topic];
-
-            var min = messages.Min(e => e.Id);
-            var max = messages.Max(e => e.Id);
-
-            if (lasted > 0 && lasted != min - 1)
+            try
             {
-                Console.WriteLine($"Wrong ID, Topic {topic}. Receive min Id = {min}, max = {max}, but lasted = {lasted}");
-                await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId, $"[{Program.Settings.Name}] Wrong ID, Topic {topic}. Receive min Id = {min}, max = {max}, but lasted = {lasted}");
-            }
+                if (messages.Count == 0)
+                    return;
 
-            if (messages.Count != max - min + 1)
+                long lasted;
+                lock (_lasted) lasted = _lasted[topic];
+
+                var min = messages.Min(e => e.Id);
+                var max = messages.Max(e => e.Id);
+
+                if (lasted > 0 && lasted != min - 1)
+                {
+                    Console.WriteLine(
+                        $"Wrong ID, Topic {topic}. Receive min Id = {min}, max = {max}, but lasted = {lasted}");
+                    await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId,
+                        $"[{Program.Settings.Name}] Wrong ID, Topic {topic}. Receive min Id = {min}, max = {max}, but lasted = {lasted}");
+                }
+
+                if (messages.Count != max - min + 1)
+                {
+                    Console.WriteLine(
+                        $"Miss messages in batch, Topic {topic}. Receive min Id = {min}, max = {max}, but count = {messages.Count}");
+                    await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId,
+                        $"[{Program.Settings.Name}] Miss messages in batch, Topic {topic}. Receive min Id = {min}, max = {max}, but count = {messages.Count}");
+                }
+
+                lock (_lasted) _lasted[topic] = max;
+
+                //Console.WriteLine($"{topic} - {messages.Count}");
+            }
+            catch (Exception e)
             {
-                Console.WriteLine($"Miss messages in batch, Topic {topic}. Receive min Id = {min}, max = {max}, but count = {messages.Count}");
-                await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId, $"[{Program.Settings.Name}] Miss messages in batch, Topic {topic}. Receive min Id = {min}, max = {max}, but count = {messages.Count}");
+                Console.WriteLine($"Exception on batch handler for {topic}\n{e}");
             }
-
-            lock (_lasted) _lasted[topic] = max;
-
-            //Console.WriteLine($"{topic} - {messages.Count}");
         }
 
         public class TopicItem
